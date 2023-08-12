2b6ac8f97437d818c58c15f5339ef6623c90ce55
DARXS <123099392+DARXS@users.noreply.github.com>
2023-08-12 09:15:26 -0300

    Update hardware_definitions.h

--- include/hardware_definitions.h
+++ include/hardware_definitions.h
@@ -4,12 +4,22 @@
 /* pinout definitions */
 // LEDS
 #define EMBEDDED_LED 2 // Turned on on failiure
+#define WAIT_LED 14 //Turned on when the button is presses of
+#define LOG_LED 12 //Turned on if the logging run
 
 //SD
 #define SD_CS 5
 
+//Debounce
+#define DEBOUNCETIME 100
+
+//Sensors
+//Pinos imaginarios, também ocorrerá mudanças
+#define freq_pin 21
+#define speed_pin 22
+
 //Button ISR
-#define Button 36
+#define Button 4
 
 //GPRS
 #define MODEM_RST 
