ef3bd144a4c376a3a74ad4537cfeb7691f8380c5
DARXS <123099392+DARXS@users.noreply.github.com>
2023-08-12 09:20:55 -0300

    Update software_definitions.h

--- include/software_definitions.h
+++ include/software_definitions.h
@@ -1,7 +1,29 @@
 #ifndef SOFT_DEFS_H_
 #define SOFT_DEFS_H_
 
-//Frequency of data acquisition
+#include <Arduino.h>
+
+//Frequency of data acquisition in Hz
 #define SAMPLE_FREQ 200
+
+//Debounce
+uint64_t saveDebounceTimeout;
+//volatile bool lastState; 
+bool saveLastState;
+bool save = false;
+
+/* State Machines */
+//Ocorrerá mudanças no enum
+typedef enum {
+    IDLE,
+    WAITING,
+    LOGGING
+} logging_states;
+
+typedef enum {} connectivity_states;
+
+logging_states l_state; // datalogger state
+
+unsigned long timer;
 
 #endif
