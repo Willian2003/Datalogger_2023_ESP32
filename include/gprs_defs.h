39b4ea8bdaf6797980082aa3375b148823e8aff2
DARXS <123099392+DARXS@users.noreply.github.com>
2023-09-04 17:02:20 -0300

    Update gprs_defs.h

--- include/gprs_defs.h
+++ include/gprs_defs.h
@@ -2,9 +2,9 @@
 #define GPRS_DEFS_H_
 
 #define SerialAT Serial2 // Set serial for AT commands (to the module)
-#define MODEM_TX 33
-#define MODEM_RX 32
-#define PORT 1883
+#define MODEM_TX 17
+#define MODEM_RX 16
+#define PORT     1883
 //#define PORT 1880 //Test for mqtt connection
 // Configure TinyGSM library
 #define TINY_GSM_MODEM_SIM800   // Modem is SIM800
