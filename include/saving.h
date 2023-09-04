7472113a435d20d8a0a0c8799748dd2d294a9db2
DARXS <123099392+DARXS@users.noreply.github.com>
2023-09-04 17:02:01 -0300

    Update saving.h

--- include/saving.h
+++ include/saving.h
@@ -9,12 +9,14 @@
 #define SAVING_PERIOD 5
 
 // if testing is needed, raising the value above may help the analisys
-
+typedef enum {
+    MOUNT_ERROR,
+    FILE_ERROR,
+    FILE_OK
+} connectivity_states;
 
 char file_name[20];
 File root;
 File dataFile;
-
-boolean running=false;
 
 #endif
